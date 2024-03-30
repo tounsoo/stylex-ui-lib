98bd61d43d93e67c9318e9ce87263938d4bd0cff
Ben Ho <tounsoo@gmail.com>
2024-03-29 22:27:59 -0700

    Add: storybook and update vite config to separate build and dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,51 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import styleX from "vite-plugin-stylex"
 import react from '@vitejs/plugin-react'
+import stylexPlugin from '@stylexjs/rollup-plugin'
 
-export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'stylex-ui-lib',
-      // the proper extensions will be added
-      fileName: 'index',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
+export default defineConfig(({command}) => {
+  if (command === 'build') {
+    return {
+      build: {
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, 'src/index.ts'),
+          name: 'stylex-ui-lib',
+          // the proper extensions will be added
+          fileName: 'index',
+        },
+        rollupOptions: {
+          // make sure to externalize deps that shouldn't be bundled
+          // into your library
+          external: ['react'],
+          output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+              react: 'React',
+            },
+          },
+          plugins: [stylexPlugin({
+            // Required. File path for the generated CSS file.
+            fileName: 'stylex.css',
+            // default: false
+            dev: false,
+            // prefix for all generated classNames
+            classNamePrefix: 'x',
+            // Required for CSS variable support
+            unstable_moduleResolution: {
+              // type: 'commonJS' | 'haste'
+              // default: 'commonJS'
+              type: 'commonJS',
+              // The absolute path to the root directory of your project
+              rootDir: __dirname,
+            },
+          })],
         },
       },
-    },
-  },
-  plugins: [react(), styleX()],
+    }
+  }
+  return {
+    plugins: [react(), styleX()]
+  }
 })
