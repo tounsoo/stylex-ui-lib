00fd619d8adcc6f0a0166f3a7a370c971597f546
Ben Ho <tounsoo@gmail.com>
2024-04-01 19:51:59 -0700

    Last direct commit to main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 export * from './component'
+
+// TODO: 
+// find a way to export tokens properly
+// issue is that stylex do not pickup the styles correctly
+// if the file extension does not end correctly
+// reference: https://stylexjs.com/docs/learn/theming/defining-variables/#1-variables-must-be-defined-in-stylexjs-files
+
+// internal source, do not export
+// export * from './types'
